b581236c8402808f0eeeff2d8796db124d864b2c
jdrescher <jdrescher@5c6c2e0e-6635-41de-94bb-d1158bea9822>
2009-02-19 01:20:27 +0000

    Continued working on port of CCmdLine
    git-svn-id: http://192.168.1.240:8084/svn/qt-basic-utils@28 5c6c2e0e-6635-41de-94bb-d1158bea9822

--- trunk/Testing/src/basic.cxx
+++ trunk/Testing/src/basic.cxx
@@ -9,6 +9,8 @@
 
 using namespace QTUTILS;
 
+/////////////////////////////////////////////////////////////////////////////////////////
+
 class QCmdTest : public QCmd
 {
 public:
@@ -40,17 +42,56 @@ int QCmdTest::Execute()
 	return QCmdParseError::STATUS_OK;
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////
+
+class QCmdGo : public QCmd
+{
+public:
+	QCmdGo(QString strName, QString strDescription);
+	virtual int Execute();
+};
+
+QCmdGo::QCmdGo(QString strName, QString strDescription) :
+QCmd(strName,strDescription)
+{
+	QStringList sl;
+	sl.push_back("0");
+	AddArg("CaseNumber(s)","The case number","",sl);
+
+	float fTemp = 0.0f;
+	AddOpt('T',"Temperature","Temperature in Fahrenheit",fTemp);
+}
+
+int QCmdGo::Execute()
+{
+
+	quint32 nCase;
+	GetArg("CaseNumber",nCase);
+
+	std::cout << "CaseNumber=" << nCase << std::endl;
+
+	float fTemp = 100.0f;
+	GetOpt('T',fTemp);
+
+	std::cout << "Temperature=" << fTemp << std::endl;
+
+	return QCmdParseError::STATUS_OK;
+}
+
+
 int main(int argc, char* argv[])
 {
 
 	try {
-		QCmdTest cmdTest("Test","This is the test command");
+		QCmdTest	cmdTest("Test","This is the test command");
+		QCmdGo		cmdGo("GO","This is the go command");
 
 		QCmdLine myCmdLine(argc,argv);
 		QCmdHelp myHelp("This command shows the help message for all commands.","");
 
 		myCmdLine.AddCmd(&myHelp);
 		myCmdLine.AddCmd(&cmdTest);
+		myCmdLine.AddCmd(&cmdGo);
 
 		myCmdLine.Parse();
 
