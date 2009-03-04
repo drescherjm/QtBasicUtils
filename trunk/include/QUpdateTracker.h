b4b6e2eeef830cd126af1290bfef8b3ac5317a20
jdrescher <jdrescher@5c6c2e0e-6635-41de-94bb-d1158bea9822>
2009-03-04 18:42:04 +0000

    Added QUpdateTracker and install.bat generation
    git-svn-id: http://192.168.1.240:8084/svn/qt-basic-utils@64 5c6c2e0e-6635-41de-94bb-d1158bea9822

---
+++ trunk/include/QUpdateTracker.h
@@ -0,0 +1,54 @@
+#ifndef __QUPDATETRACKER_H__
+#define __QUPDATETRACKER_H__
+
+namespace QTUTILS {
+
+class QUpdateTracker
+{
+public:
+	QUpdateTracker(bool bChanged=false);
+public:
+	virtual bool Modify();
+	virtual bool HasChanged();
+	virtual bool ForceUnmodified();
+private:
+	bool	m_bChanged;
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+inline QUpdateTracker::QUpdateTracker(bool bChanged) : m_bChanged(bChanged)
+{
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+inline bool QUpdateTracker::Modify()
+{
+	bool retVal = HasChanged();
+	m_bChanged = true;
+	return retVal;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+inline bool QUpdateTracker::HasChanged()
+{
+	return m_bChanged;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+inline bool QUpdateTracker::ForceUnmodified()
+{
+	bool retVal = HasChanged();
+	m_bChanged = FALSE;
+	return retVal;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+}; //namespace QTUTILS
+
+#endif //__QUPDATETRACKER_H__
