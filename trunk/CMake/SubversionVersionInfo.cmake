04d7665f1cf7abd08bd6310ec95d48ee90ace15d
jdrescher <jdrescher@5c6c2e0e-6635-41de-94bb-d1158bea9822>
2011-04-21 20:04:23 +0000

    Allow disabling subversion version check.
    git-svn-id: http://192.168.1.240:8084/svn/qt-basic-utils@174 5c6c2e0e-6635-41de-94bb-d1158bea9822

--- trunk/CMake/SubversionVersionInfo.cmake
+++ trunk/CMake/SubversionVersionInfo.cmake
@@ -3,11 +3,15 @@
 #
 FIND_PACKAGE(Subversion)
 IF(Subversion_FOUND)
-    Subversion_WC_INFO(${PROJECT_SOURCE_DIR} Project)
-    MESSAGE("Current revision is ${Project_WC_REVISION}")
-    Subversion_WC_LOG(${PROJECT_SOURCE_DIR} Project)
-    MESSAGE("Last changed log is ${Project_LAST_CHANGED_LOG}")
+	OPTION(APPEND_SVN_REV "Append subversion rev to application version" ON)
 	
-	set (${PROJECT_NAME}_VERSION_PATCH ${${PROJECT_NAME}_VERSION_PATCH}.${Project_WC_REVISION})
+	IF(APPEND_SVN_REV)
+		Subversion_WC_INFO(${PROJECT_SOURCE_DIR} Project)
+		MESSAGE("Current revision is ${Project_WC_REVISION}")
+		Subversion_WC_LOG(${PROJECT_SOURCE_DIR} Project)
+		MESSAGE("Last changed log is ${Project_LAST_CHANGED_LOG}")
+	
+		set (${PROJECT_NAME}_VERSION_PATCH ${${PROJECT_NAME}_VERSION_PATCH}.${Project_WC_REVISION})
+	ENDIF(APPEND_SVN_REV)
 	
 ENDIF(Subversion_FOUND)
