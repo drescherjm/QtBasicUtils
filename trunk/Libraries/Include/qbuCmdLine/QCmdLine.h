c48296998188d4148340a0603b0ac275c63c1e24
jdrescher <jdrescher@5c6c2e0e-6635-41de-94bb-d1158bea9822>
2014-03-22 13:18:44 +0000

    Change class names on all classes in qbuBase
    git-svn-id: http://192.168.1.240:8084/svn/qt-basic-utils@285 5c6c2e0e-6635-41de-94bb-d1158bea9822

--- trunk/Libraries/Include/qbuCmdLine/QCmdLine.h
+++ trunk/Libraries/Include/qbuCmdLine/QCmdLine.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #ifndef __QCMDLINE_H__
 #define __QCMDLINE_H__
 
@@ -7,55 +9,50 @@
 #include <QList>
 #include <QTextStream>
 
+/////////////////////////////////////////////////////////////////////////////////////////
 
+typedef QMap<QString,QCmd*> QCmdMap;
+typedef QList<QCmd*> QCmdList;
+typedef	QList<QStringList*> QStrLstList;
+
+class QCmdLine  
+{
+public:
+	QCmdLine();
+	virtual ~QCmdLine();
+	QCmdLine(int argc, char* argv[]);
+	QCmdLine( QString strProgName, QStringList & strLst );
+	QCmdLine( QTextStream & args );
+public:
+	int			Parse();
+	QString		GetSyntax();
+	int			AddCmd( QCmd* pCmd );
+	QCmdList	getAvailableCommands();
+	void		setAvailableCommands(QCmdList & lstCommands);
+
+public:
+	typedef		QStrLstList::iterator iterator;
+	iterator	begin();
+	iterator	end();
+	int			GetNextCommand(QCmdLine::iterator & it, QCmd *& pCmd);
+
+protected:
+	int			ParseFile(QString strFileName,QStringList & strLstFile);
+	int			ParseStream(QTextStream & args,QStringList & strLstFile);
+	int			PreProcess(QStringList & strList);
+	QCmd*		GetDefaultCmd();
+
+	bool		IsCmdChar(QString str);
+	QString		GetCommandString( QString strCmd);
+	bool		IsHelpCmd(QString str);
+	int			Parse( QStringList & strList);
+private:
+	void		destroy();
+private:
+	class		qtutilsPrivate;
+	qtutilsPrivate* m_pPrivate;	
+};
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
-	typedef QMap<QString,QCmd*> QCmdMap;
-	typedef QList<QCmd*> QCmdList;
-	typedef	QList<QStringList*> QStrLstList;
-
-	class QCmdLine  
-	{
-	public:
-		QCmdLine();
-		virtual ~QCmdLine();
-		QCmdLine(int argc, char* argv[]);
-		QCmdLine( QString strProgName, QStringList & strLst );
-		QCmdLine( QTextStream & args );
-	public:
-		int			Parse();
-		QString		GetSyntax();
-		int			AddCmd( QCmd* pCmd );
-		QCmdList	getAvailableCommands();
-		void		setAvailableCommands(QCmdList & lstCommands);
-
-	public:
-		typedef		QStrLstList::iterator iterator;
-		iterator	begin();
-		iterator	end();
-		int			GetNextCommand(QCmdLine::iterator & it, QCmd *& pCmd);
-
-	protected:
-		int			ParseFile(QString strFileName,QStringList & strLstFile);
-		int			ParseStream(QTextStream & args,QStringList & strLstFile);
-		int			PreProcess(QStringList & strList);
-		QCmd*		GetDefaultCmd();
-		
-		bool		IsCmdChar(QString str);
-		QString		GetCommandString( QString strCmd);
-		bool		IsHelpCmd(QString str);
-		int			Parse( QStringList & strList);
-	private:
-		void		destroy();
-	private:
-		class		qtutilsPrivate;
-		qtutilsPrivate* m_pPrivate;	
-	};
-
-/////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-#endif //__QCMDLINE_H__
+#endif // __QCMDLINE_H__
