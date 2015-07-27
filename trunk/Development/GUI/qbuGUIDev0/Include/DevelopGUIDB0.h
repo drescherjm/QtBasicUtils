aad9f76658e0569ad0292c2a6978c7b74496c431
jdrescher <jdrescher@5c6c2e0e-6635-41de-94bb-d1158bea9822>
2015-07-27 01:19:38 +0000

    Begin development of the qbuWidgets library and GUI testing.
    git-svn-id: http://192.168.1.240:8084/svn/qt-basic-utils@328 5c6c2e0e-6635-41de-94bb-d1158bea9822

--- trunk/Development/GUI/qbuGUIDev0/Include/DevelopGUIDB0.h
+++ trunk/Development/GUI/qbuGUIDev0/Include/DevelopGUIDB0.h
@@ -3,18 +3,19 @@
 #ifndef DEVELOPGUIDB0_H
 #define DEVELOPGUIDB0_H
 
-#include <QWidget>
+#include <QDialog>
 #include "qbuBase/qbuMacros.h"
 #include "ui_DevelopGUIDB0.h"
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
-class DevelopGUIDB0 : public QWidget, public Ui::DevelopGUIDB0
+class DevelopGUIDB0 : public QDialog, public Ui::DevelopGUIDB0
 {
-	QBU_DECLARE_SUPERCLASS(QWidget);
+	QBU_DECLARE_SUPERCLASS(QDialog);
 	Q_OBJECT
 public:
 	explicit DevelopGUIDB0(QWidget* parent = 0, Qt::WindowFlags f = 0);
+
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////
--- trunk/Development/GUI/qbuGUIDev0/Include/qbuGUIDev0MainWindow.h
+++ trunk/Development/GUI/qbuGUIDev0/Include/qbuGUIDev0MainWindow.h
@@ -15,6 +15,9 @@ class qbuGUIDev0MainWindow : public QMainWindow, public Ui::qbuGUIDev0MainWindow
 	QBU_DECLARE_SUPERCLASS(QMainWindow)
 public:
 	explicit qbuGUIDev0MainWindow(QWidget *parent = 0, Qt::WindowFlags flags = 0);
+
+public slots:
+	void	on_actionDBDev0_triggered();
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////
