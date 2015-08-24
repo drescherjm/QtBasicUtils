cc75f8dcd1762de27f6d15374dce07f6649fcd3e
jdrescher <jdrescher@5c6c2e0e-6635-41de-94bb-d1158bea9822>
2015-08-24 22:12:03 +0000

    Port qbuSqlQueryModel from StudyManager.
    git-svn-id: http://192.168.1.240:8084/svn/qt-basic-utils@369 5c6c2e0e-6635-41de-94bb-d1158bea9822

---
+++ trunk/Libraries/Include/qbuDataBase/qbuSqlQueryModel.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#ifndef QBUSQLQUERYMODEL_H
+#define QBUSQLQUERYMODEL_H
+
+#include "qbuDataBase/qbuDatabaseLibraryDefines.h"
+#include <QSqlQueryModel>
+#include <qbuBase/qbuMacros.h>
+#include <memory>
+
+class qbuSelectQuery;
+class qbuDatabase;
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+class qbuDataBase_EXPORT qbuSqlQueryModel : public QSqlQueryModel
+{
+	Q_OBJECT
+	QBU_DECLARE_SUPERCLASS(QSqlQueryModel)
+public:
+	explicit qbuSqlQueryModel(QObject *parent = 0);
+
+public:
+	void	setQuery(qbuSelectQuery* pQuery, std::shared_ptr<qbuDatabase> pDB);
+	using	Superclass::setQuery;
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+#endif // QBUSQLQUERYMODEL_H
