f5df0425a263ec73c5c9a049453515ed6e901c45
jdrescher <jdrescher@5c6c2e0e-6635-41de-94bb-d1158bea9822>
2016-10-21 17:36:49 +0000

    Begin to work on the logger model.
    git-svn-id: http://192.168.1.240:8084/svn/qt-basic-utils@459 5c6c2e0e-6635-41de-94bb-d1158bea9822

--- trunk/Libraries/src/qbuLog/src/qbuLoggerModel.cxx
+++ trunk/Libraries/src/qbuLog/src/qbuLoggerModel.cxx
@@ -8,6 +8,11 @@
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
+static QString Titles[] = { "Date", "Level", "File", "Message" };
+const int GRID_COLS = sizeof(Titles) / sizeof(Titles[0]);
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
 struct logData
 {
     QDateTime               m_dt;
@@ -26,9 +31,17 @@ public:
 	typedef FileNameMap::value_type				FileNameMapEntry;
     typedef std::deque<logData>					LogQueue;
 
+	enum Cols {
+		CT_DATE,
+		CT_LEVEL,
+		CT_FILENAME,
+		CT_MESSAGE
+	};
+
 public:
 
 	quint32 getFileNameIndex(const QString & strFileName);
+	QString lookupFileNameFromIndex(quint32 nIndex) const;
 
 public:
     LogQueue				m_queue;
@@ -50,6 +63,21 @@ quint32 qbuLoggerModel::qbuPrivate::getFileNameIndex(const QString & strFileName
 		retVal = m_mapFileNames.size();
 
 		m_mapFileNames.insert(FileNameMapEntry(strFileName, retVal));
+	}
+
+	return retVal;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+QString qbuLoggerModel::qbuPrivate::lookupFileNameFromIndex(quint32 nIndex) const
+{
+	QString retVal;
+
+	auto it = m_mapFileNames.right.find(nIndex);
+
+	if (it != m_mapFileNames.right.end()) {
+		retVal = it->get_left();
 	}
 
 	return retVal;
@@ -83,8 +111,73 @@ void qbuLoggerModel::logMessage(QDateTime dt, quint32 level, QString strFileName
 	data.m_level = static_cast<QxtLogger::LogLevel>(level);
 	data.m_nFileLineNumber = nLine;
 	data.m_strMsg = messages;
+	data.m_nFileIndex = m_pPrivate->getFileNameIndex(strFileName);
+
+	m_pPrivate->m_queue.emplace_back(data);
 	
 	endInsertRows();
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+int qbuLoggerModel::rowCount(const QModelIndex &parent /*= QModelIndex()*/) const
+{
+	return m_pPrivate->m_queue.size();
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+int qbuLoggerModel::columnCount(const QModelIndex &parent /*= QModelIndex()*/) const
+{
+	return GRID_COLS;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+QVariant qbuLoggerModel::headerData(int section, Qt::Orientation orientation, int role /*= Qt::DisplayRole*/) const
+{
+	Q_UNUSED(role);
+
+	QVariant retVal;
+	
+	if (orientation == Qt::Horizontal) {
+		if ((section >= 0) && (section < GRID_COLS)) {
+			retVal = Titles[section];
+		}
+	}
+
+	return retVal;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+QVariant qbuLoggerModel::data(const QModelIndex &index, int role /*= Qt::DisplayRole*/) const
+{
+	Q_UNUSED(role);
+
+	QVariant retVal;
+
+	if ( (index.row() < m_pPrivate->m_queue.size()) && (index.column() < GRID_COLS)) {
+		
+		logData& item = m_pPrivate->m_queue[index.row()];
+
+		switch (index.column()) {
+		case qbuPrivate::CT_DATE:
+			retVal = item.m_dt;
+			break;
+		case qbuPrivate::CT_LEVEL:
+			retVal = item.m_level;
+			break;
+		case qbuPrivate::CT_FILENAME:
+			retVal = QString("%1:%2").arg(m_pPrivate->lookupFileNameFromIndex(item.m_nFileIndex)).arg(item.m_nFileLineNumber);
+			break;
+		case  qbuPrivate::CT_MESSAGE:
+			retVal = item.m_strMsg;
+			break;
+		}
+	}
+	
+	return retVal;
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////
