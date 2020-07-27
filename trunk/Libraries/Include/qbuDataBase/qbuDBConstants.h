60a3cd58923933a81084130b7c526253de397768
jdrescher <jdrescher@5c6c2e0e-6635-41de-94bb-d1158bea9822>
2020-07-27 20:55:07 +0000

    Cosmetic change.
    git-svn-id: http://192.168.1.240:8084/svn/qt-basic-utils@605 5c6c2e0e-6635-41de-94bb-d1158bea9822

--- trunk/Libraries/Include/qbuDataBase/qbuDBConstants.h
+++ trunk/Libraries/Include/qbuDataBase/qbuDBConstants.h
@@ -6,11 +6,11 @@
 namespace qbudb {
 	enum InsertMode {
 		IM_NO_EXTRA_HANDLING = 0,
-		IM_ROLLBACK = 1,
-		IM_ABORT = 2,
-		IM_REPLACE = 3,
-		IM_FAIL = 4,
-		IM_IGNORE = 5
+		IM_ROLLBACK          = 1,
+		IM_ABORT             = 2,
+		IM_REPLACE           = 3,
+		IM_FAIL              = 4,
+		IM_IGNORE            = 5
 	};
 
 	enum JoinFlag {
