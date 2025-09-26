e8e2bff2f3778432a298ba3443d843c3b1066cd4
John M. Drescher <drescherjm@gmail.com>
2025-09-26 12:17:39 -0400

    Setup project to use cmake-modules installed separately using vcpkg.

--- vcpkg/ports/cmake-modules/portfile.cmake
+++ vcpkg/ports/cmake-modules/portfile.cmake
@@ -2,10 +2,11 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO drescherjm/cmake-modules
     REF bdd48bba0691d542fbf5e74e74b21af7b2ba8e09
-    SHA512 de5794314eea45dbfa96eebf513753f37887297d042b965b2ee09d1b3f593159cc3457529c129b93e5ce9f4b743571a550af2c5fd15a01e8d8f87466bc078264
+    SHA512 d124afe43b992074207738236c00ddef387d654802343e74b2c7e9a9752b26d8c71e49e9ef3994058ead1e9caf267090a88dca72dbde5b49604ba965cc528d11
 )
 
 # Install the modules and config file
-file(INSTALL ${SOURCE_PATH}/Modules DESTINATION ${CURRENT_PACKAGES_DIR}/share/cmake-modules/Modules)
-file(INSTALL ${SOURCE_PATH}/Scripts DESTINATION ${CURRENT_PACKAGES_DIR}/share/cmake-modules/Scripts)
+file(INSTALL ${SOURCE_PATH}/Modules DESTINATION ${CURRENT_PACKAGES_DIR}/share/cmake-modules)
+file(INSTALL ${SOURCE_PATH}/Scripts DESTINATION ${CURRENT_PACKAGES_DIR}/share/cmake-modules)
 file(INSTALL ${SOURCE_PATH}/cmake/cmake-modulesConfig.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/cmake-modules)
+file(INSTALL ${SOURCE_PATH}/cmake/cmake-modulesTargets.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/cmake-modules)
