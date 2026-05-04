beecc0a2defbc5fad7a5c9939046bc6870d7b821
John M. Drescher <drescherjm@gmail.com>
2026-05-04 19:52:48 -0400

    duLog: Update the log only DEBUG_BREAK() if the debugger is running otherwise a DEBUG_BREAK() was causing program termination.

--- Libraries/Include/qbuLog/qbuLog.h
+++ Libraries/Include/qbuLog/qbuLog.h
@@ -11,19 +11,90 @@
 #include <QxtLogger>
 #include <cassert>
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#if defined(_MSC_VER) || defined(_WIN32)
+#include <windows.h>
+inline bool isDebuggerPresent() {
+	return IsDebuggerPresent();
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#elif defined(__APPLE__)
+#include <sys/sysctl.h>
+#include <unistd.h>
+inline bool isDebuggerPresent() {
+	int               mib[4] = {CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()};
+	struct kinfo_proc info{};
+	size_t            size = sizeof(info);
+	sysctl(mib, 4, &info, &size, nullptr, 0);
+	return (info.kp_proc.p_flag & P_TRACED) != 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#elif defined(__linux__)
+#include <fstream>
+inline bool isDebuggerPresent() {
+	// /proc/self/status contains "TracerPid: N"
+	// N is 0 if no debugger, non-zero if a debugger is attached
+	std::ifstream status("/proc/self/status");
+	for (std::string line; std::getline(status, line);) {
+		if (line.rfind("TracerPid:", 0) == 0) {
+			int pid = std::stoi(line.substr(10));
+			return pid != 0;
+		}
+	}
+	return false;
+}
+
+#else
+// Fallback: assume no debugger
+inline bool isDebuggerPresent() {
+	return false;
+}
+#endif
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 #if defined(_MSC_VER)
-#define DEBUG_BREAK() __debugbreak()
+#define DEBUG_BREAK()                                                                                                  \
+	do {                                                                                                               \
+		if (isDebuggerPresent())                                                                                       \
+			__debugbreak();                                                                                            \
+	} while (0)
 #elif defined(__clang__)
-#define DEBUG_BREAK() __builtin_debugtrap()
+#define DEBUG_BREAK()                                                                                                  \
+	do {                                                                                                               \
+		if (isDebuggerPresent())                                                                                       \
+			__builtin_debugtrap();                                                                                     \
+	} while (0)
 #elif defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__))
-#define DEBUG_BREAK() __asm__ volatile("int $0x03")
+#define DEBUG_BREAK()                                                                                                  \
+	do {                                                                                                               \
+		if (isDebuggerPresent())                                                                                       \
+			__asm__ volatile("int $0x03");                                                                             \
+	} while (0)
 #elif defined(__GNUC__) && defined(__aarch64__)
-#define DEBUG_BREAK() __asm__ volatile("brk #0")
+#define DEBUG_BREAK()                                                                                                  \
+	do {                                                                                                               \
+		if (isDebuggerPresent())                                                                                       \
+			__asm__ volatile("brk #0");                                                                                \
+	} while (0)
 #elif defined(__GNUC__) && defined(__arm__)
-#define DEBUG_BREAK() __asm__ volatile("bkpt #0")
+#define DEBUG_BREAK()                                                                                                  \
+	do {                                                                                                               \
+		if (isDebuggerPresent())                                                                                       \
+			__asm__ volatile("bkpt #0");                                                                               \
+	} while (0)
 #else
 #include <csignal>
-#define DEBUG_BREAK() raise(SIGTRAP)
+#define DEBUG_BREAK()                                                                                                  \
+	do {                                                                                                               \
+		if (isDebuggerPresent())                                                                                       \
+			raise(SIGTRAP);                                                                                            \
+	} while (0)
 #endif
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
