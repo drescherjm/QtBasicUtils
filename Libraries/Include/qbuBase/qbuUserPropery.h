64958d74a06f682ff23d86e7c3598c17537b3c4e
John M. Drescher <drescherjm@gmail.com>
2026-05-30 10:21:21 -0400

    Temporarily add some debug information to attempt to understand a memory bug.

--- Libraries/Include/qbuBase/qbuUserPropery.h
+++ Libraries/Include/qbuBase/qbuUserPropery.h
@@ -5,34 +5,38 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#include "qbuBase/qbuUpdateTracker.h"
-#include <QObject>
-#include <QVariant>
-#include <QDomElement>
-#include <QSharedData>
 #include "qbuBase/qbuITKIndent.h"
+#include "qbuBase/qbuUpdateTracker.h"
+#include <QDomElement>
+#include <QObject>
+#include <QSharedData>
+#include <QVariant>
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-class qbuUserProperty : public QObject , public qbuUpdateTracker, public QSharedData
-{
+class qbuUserProperty : public QObject, public qbuUpdateTracker, public QSharedData {
 	Q_OBJECT
 public:
 	qbuUserProperty();
-	qbuUserProperty(const qbuUserProperty & other);
-	qbuUserProperty& operator=(const qbuUserProperty & other);
+	qbuUserProperty(const qbuUserProperty& other);
+	qbuUserProperty& operator=(const qbuUserProperty& other);
+	virtual ~qbuUserProperty();
+
 public:
-	virtual QString		toXML(bool bMakeRoot = true,qbuITKIndent indent = qbuITKIndent())=0;
-	virtual bool		fromXML(QString strXML)=0;
-	virtual bool		fromXML(QDomElement & domElem)=0;
-	virtual bool		Load(QString strFile)=0;
-	virtual bool		Save(QString strFile)=0;
+	virtual QString toXML(bool bMakeRoot = true, qbuITKIndent indent = qbuITKIndent()) = 0;
+	virtual bool    fromXML(QString strXML)                                            = 0;
+	virtual bool    fromXML(QDomElement& domElem)                                      = 0;
+	virtual bool    Load(QString strFile)                                              = 0;
+	virtual bool    Save(QString strFile)                                              = 0;
+
 private:
 };
 
+
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//Q_DECLARE_METATYPE(UserProperty);
+// Q_DECLARE_METATYPE(UserProperty);
 Q_DECLARE_METATYPE(qbuUserProperty*);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
